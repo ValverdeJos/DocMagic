5239e66a5e7dea544ac66f08fc1b0349b1ab9b32
José Luis <josefrangaocarlos4@gmail.com>
2023-05-08 10:36:42 +0100

    information for the code

--- ExporterPub.py
+++ ExporterPub.py
@@ -7,7 +7,7 @@ desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
 os.chdir(desktop_path)
 
 
-input_fileGlobal = "C:\\Users\\josel\\OneDrive\\Bureau\\ProjectExporterPub\\FICHASTECNICAS2023.pub"
+input_fileGlobal = " "
 pdf_folderGlobal = desktop_path+"\\PdfsExported"
 pdf_Global = pdf_folderGlobal+"\\GlobalPDF.pdf"
 output_folder = pdf_folderGlobal +"\\pages"
@@ -18,22 +18,22 @@ def convert_pub_to_pdf(input_file: str, pdf_folder: str) -> str:
 
     publisher = win32com.client.Dispatch("Publisher.Application")
 
-    # Open the input file
+    # Abrir o Ficheiro de entrada
     document = publisher.Open(input_file)
 
-    # Export each page to a separate PDF file
-
-    # Create a file name for the PDF
+    
+    # Criar o nome do fichiero para o PDF
     pdf_name = "GlobalPDF.pdf"
     pdf_path = os.path.join(pdf_folder, pdf_name)
 
-    # Export the current page to PDF
+    # exportar a pagina currente do PDF
     try:
         document.ExportAsFixedFormat(2, pdf_path)
     except:
-        pass
+        print("Error in Exporting Pdf")
 
-    # Close the document and Publisher application
+   
+    # Fechar o Document e o Publisher
     document.Close()
     publisher.Quit()
     
@@ -44,23 +44,24 @@ def cut_pdf_to_pages(path_PDF=pdf_Global):
         os.makedirs(output_folder)
 
     with open(path_PDF, 'rb') as pdf_file:
-        # Créer un lecteur PDF
+        # Criar un letor de Pdf
         pdf_reader = PdfReader(pdf_file)
 
-        # Obtenir le nombre de pages
+        # Obter todas as Paginas
         num_pages = len(pdf_reader.pages)
         print(num_pages)
         for page_num in range(num_pages):
-            # Créer un écrivain PDF
+            # Criar un escritor de Pdf
             pdf_writer = PdfWriter()
 
-            # Obtenir la page
+            # Obter a pagina
             page = pdf_reader.pages[page_num]
 
-            # Ajouter la page à l'écrivain PDF
+            
+            # Adicionar a pagina o escritor PDF
             pdf_writer.add_page(page)
 
-            # Créer le nouveau fichier PDF
+            # Criar um Novo Ficheiro PDF
             new_file_name = 'page' + str(page_num) + '.pdf'
             new_file = open(os.path.join(output_folder, new_file_name), 'wb')
             Way_single_pdf = f"{output_folder}\\{new_file_name}"
@@ -71,15 +72,12 @@ def cut_pdf_to_pages(path_PDF=pdf_Global):
             extra_Text_Pdf(Way_single_pdf)
             print(page_num)
             
-            
-
 def extra_Text_Pdf(file):
+    text=''
     pdf_file_extra_Text_and_Image = open(file,'rb')
     pdf_reader_extra_Text_and_Image = PdfReader(pdf_file_extra_Text_and_Image)
-
-    meta = pdf_reader_extra_Text_and_Image.metadata
     
-    text=''
+    
     for page in range(1):
         text += pdf_reader_extra_Text_and_Image.pages[page].extract_text()
     first_line=text.splitlines()[0]
@@ -98,17 +96,6 @@ def extra_Text_Pdf(file):
     
     pdf_file_extra_Text_and_Image.close()
     os.rename(file, Way_return_pdf)
-
-    
-def test():
-    # Créer un nouveau dossier sur le bureau de l'utilisateur
-    print(pdf_folderGlobal)
-    print(pdf_Global)
-    print(output_folder)
-    
-
-
-    
 
 def get_pdf_pages_from_pub(pub_location=input_fileGlobal, save_pdf_location=pdf_folderGlobal):
     pdf_path = convert_pub_to_pdf(pub_location, save_pdf_location)
