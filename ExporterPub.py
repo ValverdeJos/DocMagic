1d7cef1ed51de624e4341320fe94312451de6412
José Valverde <127839681+Hivy-lab@users.noreply.github.com>
2023-05-15 16:29:13 +0100

    Add files via upload

---
+++ ExporterPub.py
@@ -0,0 +1,108 @@
+import os
+import win32com.client
+from PyPDF2 import  PdfReader, PdfWriter
+
+desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+os.chdir(desktop_path)
+
+
+input_fileGlobal = " "
+pdf_folderGlobal = desktop_path+"\\PdfsExported"
+pdf_Global = pdf_folderGlobal+"\\GlobalPDF.pdf"
+output_folder = pdf_folderGlobal +"\\pages"
+
+def convert_pub_to_pdf(input_file: str, pdf_folder: str) -> str:  
+    if not os.path.exists(pdf_folder):
+        os.makedirs(pdf_folder)
+
+    publisher = win32com.client.Dispatch("Publisher.Application")
+
+    # Abrir o Ficheiro de entrada
+    document = publisher.Open(input_file)
+
+    
+    # Criar o nome do fichiero para o PDF
+    pdf_name = "GlobalPDF.pdf"
+    pdf_path = os.path.join(pdf_folder, pdf_name)
+
+    # exportar a pagina currente do PDF
+    try:
+        document.ExportAsFixedFormat(2, pdf_path)
+    except:
+        pass
+        # print("Error in Exporting Pdf")
+
+   
+    # Fechar o Document e o Publisher
+    document.Close()
+    publisher.Quit()
+    
+    return pdf_path
+
+def cut_pdf_to_pages(path_PDF=pdf_Global):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    with open(path_PDF, 'rb') as pdf_file:
+        # Criar un letor de Pdf
+        pdf_reader = PdfReader(pdf_file)
+
+        # Obter todas as Paginas
+        num_pages = len(pdf_reader.pages)
+        # print(num_pages)
+        for page_num in range(num_pages):
+            # Criar un escritor de Pdf
+            pdf_writer = PdfWriter()
+
+            # Obter a pagina
+            page = pdf_reader.pages[page_num]
+
+            
+            # Adicionar a pagina o escritor PDF
+            pdf_writer.add_page(page)
+
+            # Criar um Novo Ficheiro PDF
+            new_file_name = 'page' + str(page_num) + '.pdf'
+            new_file = open(os.path.join(output_folder, new_file_name), 'wb')
+            Way_single_pdf = f"{output_folder}\\{new_file_name}"
+            pdf_writer.write(new_file)
+            new_file.close()
+
+    
+            extra_Text_Pdf(Way_single_pdf)
+            # print(page_num)
+            
+def extra_Text_Pdf(file):
+    text=''
+    pdf_file_extra_Text_and_Image = open(file,'rb')
+    pdf_reader_extra_Text_and_Image = PdfReader(pdf_file_extra_Text_and_Image)
+    
+    
+    for page in range(1):
+        text += pdf_reader_extra_Text_and_Image.pages[page].extract_text()
+    first_line=text.splitlines()[0]
+    name_Title_PDF= first_line.split("|")
+    counter = 0
+    while True:
+        if counter == 0:
+            Way_return_pdf = f"{output_folder}\\{name_Title_PDF[0]}.pdf"
+        else:
+            Way_return_pdf = f"{output_folder}\\{name_Title_PDF[0]}_{counter}.pdf"
+        
+        if not os.path.exists(Way_return_pdf):
+            break
+        counter += 1
+    # print(Way_return_pdf)
+    
+    pdf_file_extra_Text_and_Image.close()
+    os.rename(file, Way_return_pdf)
+
+def get_pdf_pages_from_pub(pub_location=input_fileGlobal, save_pdf_location=pdf_folderGlobal):
+    pdf_path = convert_pub_to_pdf(pub_location, save_pdf_location)
+    cut_pdf_to_pages(pdf_path)
+
+def get_pdf_SimplePage_from_pub(pub_location=input_fileGlobal, save_pdf_location=pdf_folderGlobal):
+    convert_pub_to_pdf(pub_location, save_pdf_location)
+    
+
+    
---
+++ MainInterface.py
@@ -0,0 +1,392 @@
+# coding:utf-8
+import os
+import sys
+from tkinter import filedialog
+from PyQt5.QtCore import Qt, QRect
+from PyQt5.QtGui import QIcon, QPainter, QImage, QBrush, QColor, QFont,QPixmap
+from PyQt5.QtWidgets import QApplication, QFrame, QStackedWidget, QHBoxLayout, QLabel,QPushButton,QCheckBox
+import qtawesome as qta
+import ExporterPub as ConverterPDF
+import ExporterExecl as ConverterExcel
+
+
+from qfluentwidgets import (NavigationInterface, NavigationItemPosition, NavigationWidget, MessageBox,
+                            isDarkTheme, setTheme, Theme)
+from qfluentwidgets import FluentIcon as FIF
+from qframelesswindow import FramelessWindow, StandardTitleBar
+
+extention = [".xls",".xlsm",".xlsx",".xlt",".xlsb",".xltx",".xltm"]
+desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+
+class Widget(QFrame):
+
+    def __init__(self, text: str, parent=None):
+        super().__init__(parent=parent)
+        
+        self.label = QLabel(text, self)
+        self.label.setAlignment(Qt.AlignCenter)
+        self.hBoxLayout = QHBoxLayout(self)
+        self.hBoxLayout.addWidget(self.label, 1, Qt.AlignCenter)
+        self.setObjectName(text.replace(' ', '-'))
+
+
+class AvatarWidget(NavigationWidget):
+    """ Avatar widget """
+
+    def __init__(self, parent=None):
+        super().__init__(isSelectable=False, parent=parent)
+        self.avatar = QImage(f'{desktop_path}\\PubExporterPdf\\resource\\shoko.png').scaled(
+            24, 24, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+
+    def paintEvent(self, e):
+        painter = QPainter(self)
+        painter.setRenderHints(
+            QPainter.SmoothPixmapTransform | QPainter.Antialiasing)
+
+        painter.setPen(Qt.NoPen)
+
+        if self.isPressed:
+            painter.setOpacity(0.7)
+
+        # draw background
+        if self.isEnter:
+            c = 255 if isDarkTheme() else 0
+            painter.setBrush(QColor(c, c, c, 10))
+            painter.drawRoundedRect(self.rect(), 5, 5)
+
+        # draw avatar
+        painter.setBrush(QBrush(self.avatar))
+        painter.translate(8, 6)
+        painter.drawEllipse(0, 0, 24, 24)
+        painter.translate(-8, -6)
+
+        if not self.isCompacted:
+            painter.setPen(Qt.white if isDarkTheme() else Qt.black)
+            font = QFont('Segoe UI')
+            font.setPixelSize(14)
+            painter.setFont(font)
+            painter.drawText(QRect(44, 0, 255, 36), Qt.AlignVCenter, 'User')
+
+
+class Window(FramelessWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setTitleBar(StandardTitleBar(self))
+
+        setTheme(Theme.DARK)
+
+
+        self.hBoxLayout = QHBoxLayout(self)
+        self.navigationInterface = NavigationInterface(self, showMenuButton=True)
+        self.stackWidget = QStackedWidget(self)
+        self.iconWord = qta.icon('fa.file-word-o', color='white',scale_factor=1.1)
+        self.iconPDF = qta.icon('mdi.file-pdf', color='white',scale_factor=1.3)
+        self.iconExcel = qta.icon('mdi.file-excel', color='white',scale_factor=1.3)
+
+
+        # Ajouter un QLabel pour l'image
+        self.imageLabel = QLabel(self)
+        self.imageLabel.setPixmap(QPixmap(f"{desktop_path}\\PubExporterPdf\\resource\\assets\\Xls.png").scaled(200, 200))
+        self.imageLabel.move(350, 170)
+        self.imageLabel.resize(300, 300)
+
+        self.upload = QPushButton("Escolha o seu Ficheiro", self)
+        self.upload.move(230, 580)
+        self.upload.resize(160, 45)
+        self.upload.setObjectName("upload")
+        self.upload.clicked.connect(self.UploadFiles)
+
+        self.confirm = QPushButton("Confirma", self)
+        self.confirm.move(570, 580)
+        self.confirm.resize(160, 45)
+        self.confirm.setObjectName("confirm")
+        self.confirm.clicked.connect(self.run_code)
+
+        self.checkBoxSplit = QCheckBox("Separa ficheiro ",self)
+        self.checkBoxSplit.move(245, 545)
+        self.checkBoxSplit.resize(120, 30)
+        self.checkBoxSplit.setObjectName("checkBoxSplit")
+        self.checkBoxSplit.stateChanged.connect(self.CheckText)
+
+
+        with open(f"{desktop_path}\\PubExporterPdf\\resource\\style.css", "r") as f:
+            style_sheet = f.read()
+        self.setStyleSheet(style_sheet)
+
+        # create sub interface
+        self.pubInterface = Widget('File Publisher', self)
+        self.excelInterface = Widget('File Excel', self)
+        self.pdfInterface = Widget('File PDF', self)
+        self.wordInterface = Widget('File Word', self)
+        self.folderInterface = Widget('Folder Interface', self)
+        self.settingInterface = Widget('Setting Interface', self)
+        
+
+        # initialize layout
+        self.initLayout()
+
+        # add items to navigation interface
+        self.initNavigation()
+
+        self.initWindow()
+
+
+
+
+    def initLayout(self):
+        self.hBoxLayout.setSpacing(0)
+        self.hBoxLayout.setContentsMargins(0, self.titleBar.height(), 0, 0)
+        self.hBoxLayout.addWidget(self.navigationInterface)
+        self.hBoxLayout.addWidget(self.stackWidget)
+        self.hBoxLayout.setStretchFactor(self.stackWidget, 1)
+
+    def initNavigation(self):
+        
+        self.addSubInterface(self.pubInterface, FIF.DOCUMENT, 'Publisher')
+        self.addSubInterface(self.excelInterface, self.iconExcel, 'Excel')
+        self.addSubInterface(self.pdfInterface, self.iconPDF , 'PDF')
+        self.addSubInterface(self.wordInterface, self.iconWord, 'Word')
+
+
+        self.navigationInterface.addSeparator()
+
+        # add navigation items to scroll area
+        self.addSubInterface(self.folderInterface, FIF.FOLDER, 'Folder library', NavigationItemPosition.SCROLL)
+        # for i in range(1, 21):
+        #     self.navigationInterface.addItem(
+        #         f'folder{i}',
+        #         FIF.FOLDER,
+        #         f'Folder {i}',
+        #         lambda: print('Folder clicked'),
+        #         position=NavigationItemPosition.SCROLL
+        #     )
+
+        # add custom widget to bottom
+        self.navigationInterface.addWidget(
+            routeKey='avatar',
+            widget=AvatarWidget(),
+            onClick=self.showMessageBox,
+            position=NavigationItemPosition.BOTTOM
+        )
+
+        self.addSubInterface(self.settingInterface, FIF.SETTING, 'Settings', NavigationItemPosition.BOTTOM)
+
+        #!IMPORTANT: don't forget to set the default route key if you enable the return button
+        # self.navigationInterface.setDefaultRouteKey(self.musicInterface.objectName())
+
+        # set the maximum width
+        # self.navigationInterface.setExpandWidth(300)
+
+        self.stackWidget.currentChanged.connect(self.onCurrentInterfaceChanged)
+        self.stackWidget.setCurrentIndex(1)
+
+    def initWindow(self):
+        
+        self.resize(900, 700)
+        self.setWindowIcon(QIcon('f"{desktop_path}\\PubExporterPdf\\resource\\LogoPng.png'))
+        self.setWindowTitle('DocMagic')
+        self.titleBar.setAttribute(Qt.WA_StyledBackground)
+
+        desktop = QApplication.desktop().availableGeometry()
+        w, h = desktop.width(), desktop.height()
+        self.move(w//2 - self.width()//2, h//2 - self.height()//2)
+
+        self.setQss()
+
+        self.pubInterface.setHidden(True)
+        self.excelInterface.setHidden(True)
+        self.pdfInterface.setHidden(True)
+        self.wordInterface.setHidden(True)
+        self.folderInterface.setHidden(True)
+        self.settingInterface.setHidden(True)
+
+    def addSubInterface(self, interface, icon, text: str, position=NavigationItemPosition.TOP):
+        """ add sub interface """
+        self.stackWidget.addWidget(interface)
+        self.navigationInterface.addItem(
+            routeKey=interface.objectName(),
+            icon=icon,
+            text=text,
+            onClick=lambda: self.switchTo(interface),
+            position=position,
+            tooltip=text
+        )
+
+    def setQss(self):
+
+        color = 'dark' if isDarkTheme() else 'light'
+        with open(f'{desktop_path}\\PubExporterPdf\\resource\\{color}\\demo.qss', encoding='utf-8') as f:
+            self.setStyleSheet(f.read())
+
+
+    def switchTo(self, widget):
+        self.stackWidget.setCurrentWidget(widget)
+
+    def onCurrentInterfaceChanged(self, index):
+        widget = self.stackWidget.widget(index)
+        arrayName = widget.objectName()
+        Name = arrayName.split('-')
+        pathImages={
+            "Excel": f"{desktop_path}\\PubExporterPdf\\assets\\Xls.png",
+            "Publisher": f"{desktop_path}\\PubExporterPdf\\assets\\Pub.png",
+            "PDF": f"{desktop_path}\\PubExporterPdf\\assets\\Pdf.png",
+            "Word": f"{desktop_path}\\PubExporterPdf\\assets\\Word.png"
+            }
+
+
+        self.upload.pyqtConfigure(text=f"Escolha o seu {Name[1]}")
+        #print(index)
+
+        if Name[1] in pathImages:
+            pathImage = pathImages[Name[1]]
+            self.imageLabel.setPixmap(QPixmap(pathImage).scaled(200,200))
+            if Name[1] == "Word":
+                self.Feature = MessageBox(
+                    'New Feature',
+                    'Está feature vai estar brevement disponivel 😉',
+                    self
+                )
+                self.Feature.exec()  
+            self.textBreve = QLabel("Funcionalidade Brevemente disponivel", self) 
+            self.textBreve.resize(300, 30)
+            self.textBreve.move(350,500)
+            self.textBreve.setObjectName("Feature")
+        else:
+            self.imageLabel.clear()
+
+        if index == 4:
+            
+            self.upload.setHidden(True)
+            self.confirm.setHidden(True)
+            self.checkBoxSplit.setHidden(True)
+            self.folderInterface.setHidden(False)
+            self.textBreve.setHidden(True)
+            
+        elif index == 5:
+            self.settingInterface.setHidden(False)
+            self.upload.setHidden(True)
+            self.confirm.setHidden(True)
+            self.textBreve.setHidden(True)
+            self.checkBoxSplit.setHidden(True)
+
+        elif index == 3:
+            self.wordInterface.setHidden(False)
+            self.upload.setHidden(True)
+            self.confirm.setHidden(True)
+            self.textBreve.setHidden(False)
+            self.checkBoxSplit.setHidden(True)
+
+        else:
+            self.upload.setHidden(False)
+            self.confirm.setHidden(False)
+            self.checkBoxSplit.setHidden(False)
+            self.pubInterface.setHidden(True)
+            self.excelInterface.setHidden(True)
+            self.pdfInterface.setHidden(True)
+            self.textBreve.setHidden(True)
+            
+            
+          
+    def showMessageBox(self):
+        w = MessageBox(
+            'New feature ',
+            'Isto é uma feature que vais ser desenvolvida em breve 😉',
+            self
+        )
+        w.exec()
+
+    def UploadFiles(self):
+            global file_path
+            file_path = filedialog.askopenfilename()
+
+    def CheckText(self):
+        if self.checkBoxSplit.isChecked():
+            self.checkBoxSplit.pyqtConfigure(text="Separa ficheiro On")
+        else:
+            self.checkBoxSplit.pyqtConfigure(text="Separa ficheiro Off")
+
+    def run_code(self):
+        self.MessageSplit = MessageBox(
+            'Tarefa Concluida',
+            'A Tarefa foi concluida com sucesso Pdfs Separados 😉',
+            self
+        )
+        self.MessageNoSplit = MessageBox(
+            'Tarefa Concluida',
+            'A Tarefa foi concluida com sucesso Pdfs não separados 😉',
+            self
+        )
+        self.MessageError = MessageBox(
+            'Error',
+            'Error: file_path esta vazio',
+            self
+        )
+        self.MessageErrorExecl = MessageBox(
+            'Error',
+            'Não é um fichiero Excel.',
+            self
+        )
+        
+        _, file_extension = os.path.splitext(file_path)
+
+        if file_extension == ".pub":
+            if self.checkBoxSplit.isChecked():
+
+                try:
+                    ConverterPDF.get_pdf_pages_from_pub(pub_location=file_path)
+                    #messagebox.showinfo("Sucesso","Converção em Pdfs Separados foi um sucesso")
+                    self.MessageSplit.exec()
+                except:
+                    self.MessageError.exec()
+            else:
+
+                try:
+                    ConverterPDF.get_pdf_SimplePage_from_pub(pub_location=file_path)
+                    #messagebox.showinfo("Sucesso","Converção em Pdf foi um sucesso")
+                    self.MessageNoSplit.exec()
+                except:
+                    self.MessageError.exec()
+        elif file_extension == ".pdf":
+
+            try:
+                ConverterPDF.cut_pdf_to_pages(path_PDF=file_path)
+                self.MessageNoSplit.exec()
+            except:
+                self.MessageError.exec()
+
+        elif file_extension in extention :
+
+            if self.checkBoxSplit.isChecked():
+
+                if file_path:
+                    try:
+                        ConverterExcel.get_pdf_pages_from_Excel(pub_location=file_path)
+                        self.MessageSplit.exec()
+                    except:
+                        self.MessageError.exec()
+                else:
+                    self.MessageErrorExecl.exec()
+
+            else:
+                if file_path:
+                    try:
+                        ConverterExcel.get_pdf_SimplePage_from_Excel()
+                        self.MessageNoSplit.exec()
+                    except:
+                        self.MessageError.exec()
+                else:
+                    self.MessageErrorExecl.exec()
+        
+
+if __name__ == '__main__':
+    
+    QApplication.setHighDpiScaleFactorRoundingPolicy(
+        Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+
+    app = QApplication(sys.argv)
+    w = Window()
+    w.show()
+    app.exec_()
